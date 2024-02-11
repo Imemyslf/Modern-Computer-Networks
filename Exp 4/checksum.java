e86d44e86f13199f42d675691bf524c49f311ae1
Imemyslf <kishanbro9666@gmail.com>
2024-02-12 00:17:00 +0530

    checksum

--- Exp 4/checksum.java
+++ Exp 4/checksum.java
@@ -3,41 +3,32 @@ import java.util.*;
 public class checksum {
     
     public int carry = 0;
-    @SuppressWarnings("unchecked")
-    public ArrayList<Integer> remain(ArrayList<Integer> newsegment1, ArrayList<Integer> newsegment2, ArrayList<Integer> remainder,int blocksize) {
-        int j;
-        remainder.clear();
-        for ( j = 0; j < blocksize; j++){
-            if (newsegment1.get(j) == 0 && newsegment2.get(j) == 0 && carry == 0){
+    // @SuppressWarnings("unchecked")
+    public ArrayList<Integer> remain(ArrayList<Integer> newsegment1, ArrayList<Integer> newsegment2, ArrayList<Integer> remainder, int blocksize) {
+        
+        for (int j = 0; j < blocksize; j++) {
+            if (newsegment1.get(j) == 0 && newsegment2.get(j) == 0 && carry == 0) {
                 remainder.add(0);
                 carry = 0;
-            }
-            else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 0 && carry == 1){
+            } else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 0 && carry == 1) {
                 remainder.add(1);
                 carry = 0;
-            }
-            else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 1 && carry == 0){
+            } else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 1 && carry == 0) {
                 remainder.add(1);
                 carry = 0;
-            }
-            else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 1 && carry == 1){
+            } else if (newsegment1.get(j) == 0 && newsegment2.get(j) == 1 && carry == 1) {
                 remainder.add(0);
                 carry = 1;
-            }
-            else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 0 && carry == 0){
+            } else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 0 && carry == 0) {
                 remainder.add(1);
                 carry = 0;
-            }
-            else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 0 && carry == 1){
+            } else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 0 && carry == 1) {
                 remainder.add(0);
                 carry = 1;
-            }
-            else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 1 && carry == 0){
+            } else if (newsegment1.get(j) == 1 && newsegment2.get(j) == 1 && carry == 0) {
                 remainder.add(0);
                 carry = 1;
-            }
-            else
-            {
+            } else {
                 remainder.add(1);
                 carry = 1;
             }
@@ -45,62 +36,66 @@ public class checksum {
         Collections.reverse(remainder);
         return remainder;
     }
+    
     public static void main(String[] args) {
 
         checksum cs = new checksum();
-        int i, j,ptr = 0;
+        int ptr = 0;
         int blocksize = 8;
         int numberofblocks = 4;
         int[] data = {1,0,1,1,0,0,1,1,1,0,1,0,1,0,1,1,0,1,0,1,1,0,1,0,1,1,0,1,0,1,0,1}; 
         ArrayList<Integer>[] segment = (ArrayList<Integer>[]) new ArrayList[numberofblocks];
         ArrayList<Integer> remainder1 = new ArrayList<>(); 
-        ArrayList<Integer> remainder2 = new ArrayList<>(); 
         ArrayList<Integer> buffer = new ArrayList<>(); 
         ArrayList<Integer> newsegment1 = new ArrayList<>(); 
         ArrayList<Integer> newsegment2 = new ArrayList<>(); 
 
-        for (i = 0; i < numberofblocks - 1; i++) {
+        for (int i = 0; i < blocksize - 1; i++) {
             buffer.add(0);
         }
         buffer.add(1);
-
-        for (i = 0; i < numberofblocks; i++) {
+        
+        for (int i = 0; i < numberofblocks; i++) {
             segment[i] = new ArrayList<Integer>();
         }
 
-        for (i = 0; i < numberofblocks; i++) {
-            for (j = ptr; j < blocksize; j++) {
+        for (int i = 0; i < numberofblocks; i++) {
+            for (int j = ptr; j < ptr + blocksize; j++) {
                 segment[i].add(data[j]);
             }
-            ptr = j;
-            blocksize = blocksize + 8;
+            ptr += blocksize;
         }
 
-        for (i = 0; i < numberofblocks; i++) {
+        for (int i = 0; i < numberofblocks; i++) {
             System.out.println(segment[i] + "\n");
         }
         
-        newsegment1 = new ArrayList<Integer> (segment[0]);
-        newsegment2 = new ArrayList<Integer> (segment[1]);
-        i = 0;
-        ptr = 1;
+        newsegment1 = new ArrayList<Integer>(segment[0]);
+        newsegment2 = new ArrayList<Integer>(segment[1]);
+        
         int flag = 1;
+        ptr = 1;
         while(flag == 1){
-            // segment[i] = new ArrayList<Integer> (newsegment);
-            // segment[ptr] = new ArrayList<Integer> (segment[ptr]);
+            Collections.reverse(newsegment1);
+            Collections.reverse(newsegment2);
             System.out.println(newsegment1);
             System.out.println(newsegment2);
             
-            if (ptr == numberofblocks){
-                flag = 0;
-            }
-            else
+            if (ptr == 2){
+                break;
+            } 
+            else 
             {
-                remainder1 = cs.remain(newsegment1,newsegment2,remainder1,blocksize); 
-                if ( cs.carry == 1){
-                    remainder1 = cs.remain(remainder1,buffer,remainder2,blocksize);
-                }
+                remainder1.clear();
+                remainder1 = cs.remain(newsegment1, newsegment2, remainder1, blocksize); 
+                System.out.println(remainder1);
+                System.out.println(cs.carry);
+                
+                // if (cs.carry == 1) {
+                //     // Handle carry if needed
+                // }
             }
+            ptr++;
         }
     }
 }

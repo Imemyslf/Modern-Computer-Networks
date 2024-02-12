daf6162bf6f6fe7a8da3c2f24aece5f330e6fc75
Imemyslf <kishanbro9666@gmail.com>
2024-02-12 23:16:14 +0530

    addedcomments

--- Exp 3/CRC.java
+++ Exp 3/CRC.java
@@ -1,83 +1,123 @@
 import java.util.*;
 
-class CRC {
+class CRC 
+{
    
+    //Initialization of ptr as global variable to toggle around the errordetection() function and remain() function effectively.
     private int ptr;
+    
+    // Initialization of dividend and divisor as gloabl list.
     public ArrayList<Integer> dividend = new ArrayList<Integer>();
     public ArrayList<Integer> divisor = new ArrayList<Integer>();
 
-    public ArrayList<Integer> remain(ArrayList<Integer> remainder,ArrayList<Integer> divid) {
-                remainder.remove(0);
-                remainder.add(dividend.get(ptr));
-                divid.clear();
-                divid = new ArrayList<>(remainder);
-                return divid;
+    // remain() function.
+    public ArrayList<Integer> remain(ArrayList<Integer> remainder,ArrayList<Integer> divid) 
+    {
+        //Remove the first elemnt of remainder.        
+        remainder.remove(0);
+        // Add ptr to remainder.
+        remainder.add(dividend.get(ptr));
+        // Clearing divid list for further operations.
+        divid.clear();
+        // Initialize divid = remainder for further opreations.
+        divid = new ArrayList<>(remainder);
+        return divid;
     }
-    public void CRC_codegenerator(int m,int n){
+
+    // CRC_codegenerator() function.
+    public void CRC_codegenerator(int m,int n)
+    {
         int i;
-        ArrayList<Integer> divid = new ArrayList<Integer> (dividend);
-        ArrayList<Integer> divir = new ArrayList<Integer> (divisor);
-        ArrayList<Integer> remainder = new ArrayList<>();
-        ArrayList<Integer> quotient = new ArrayList<>();
+        ArrayList<Integer> divid = new ArrayList<Integer> (dividend); // Initializing temporary dividend list for calculation purpose.
+        ArrayList<Integer> divir = new ArrayList<Integer> (divisor); // Initializing temporary dividend list for calculation purpose.
+        ArrayList<Integer> remainder = new ArrayList<>(); // creating a quotient list to add the quotient elements.
+        ArrayList<Integer> quotient = new ArrayList<>(); // creating a remainder list to add the remainder elements.
         
-        for (int k = 0; k < (( dividend.size() - divisor.size() ) + 1); k++){
-            quotient.add(divid.get(0));
-            if (divid.get(0) == 1){
+        // The loop will run till (dividend.size() - divisor.size()) + 1 i.e Example (8 - 4) + 1 = 5 times.
+        for (int k = 0; k < (( dividend.size() - divisor.size() ) + 1); k++)
+        {
+            quotient.add(divid.get(0)); // Adding the first element of divid to quotient.
+            
+            // If first element is 1 the go inside the if statement else if first element divid = 0 then in else part.
+            if (divid.get(0) == 1)
+            {
                 
+                // Initialize divir = dividend. Since we will clear divir multiple times so we have initialize divir = divisor when first element of divid is 1.
                 divir = new ArrayList<>(divisor);
                 
-                for ( i=0; i < divisor.size(); i++){
+                // Applying xor opreation on divid and divr and add it to remainder.
+                for ( i=0; i < divisor.size(); i++)
+                {
                     
                     remainder.add(divid.get(i) ^ divir.get(i));
                 }
                 
-                if ( k == 0){
+                //if it is first iteration when divid = 1 then...
+                if ( k == 0)
+                {
+                    //initialize ptr = i to add the next elemt of dividend.
+                    ptr = i;    
                     
-                    ptr = i;                    
+                    //Calling remain() function and assigning the value to newly obtained divid.                
                     divid = remain(remainder,divid);
+
+                    //Clearing remainder and divid to initialize the new values for further opreations.
                     remainder.clear();
                     divir.clear();
                 }
-
-                else{
-                    
+                else
+                {
+                    // Incrementing ptr.
                     ptr = this.ptr + 1;
-                    if (ptr != dividend.size()){                            
-                    divid = remain(remainder,divid);
-                    remainder.clear();
-                    divir.clear();
+
+                    // If ptr != dividend.size() then i.e ptr != 8(example) then do the opreations. When ptr == 8 dont do  anything.
+                    if (ptr != dividend.size())
+                    {   
+                        //Calling remain() function and assigning the value to newly obtained divid.                         
+                        divid = remain(remainder,divid);
+                        //Clearing remainder and divid to initialize the new values for further opreations.
+                        remainder.clear();
+                        divir.clear();
                     }
                 }    
             }
 
-            else {
-                divir.clear();
-                
-                for ( i=0; i < divisor.size(); i++){
-                    
+            else 
+            {
+                // If the first element of divid is 0 then initialize divir to zero.
+                for ( i=0; i < divisor.size(); i++)
+                {
                     divir.add(0);
                 }
                 
-                for ( i=0; i < divisor.size(); i++){
-                    
+                // Applying xor opreation on divid and divr and add it to remainder.
+                for ( i=0; i < divisor.size(); i++)
+                {
                     remainder.add(divid.get(i) ^ divir.get(i));
                 }
 
-                if ( k == 0){
-                    
+                //if it is first iteration when divid = 0 then...
+                if ( k == 0)
+                {
+                    //initialize ptr = i to add the next elemt of dividend.
                     ptr = i;
+                    //Calling remain() function and assigning the value to newly obtained divid.
                     divid = remain(remainder,divid);
+                    //Clearing remainder and divid to initialize the new values for further opreations.
                     remainder.clear();
                     divir.clear();
                 }
                 
-                else {
-                    
+                else 
+                {
+                    // Incrementing ptr.
                     ptr = this.ptr + 1;
                     
+                    // If ptr != dividend.size() then i.e ptr != 8(example) then do the opreations. When ptr == 8 dont do  anything.
                     if (ptr != dividend.size()){                            
-                        
+                        //Calling remain() function and assigning the value to newly obtained divid.
                         divid = remain(remainder,divid);
+                        //Clearing remainder and divid to initialize the new values for further opreations.
                         remainder.clear();
                         divir.clear();
                     }  
@@ -86,19 +126,23 @@ class CRC {
             }
         }
     
+        //Initializing crccode list to finnal remainder.
         ArrayList<Integer> crccode = new ArrayList <Integer> (remainder);
         crccode.remove(0);
 
-        for ( i = (dividend.size() -1) ; i > m -1 ; i--) {
-            
+        // Removing the k - 1  bits that were added in the beginnging.
+        for ( i = (dividend.size() -1) ; i > m -1 ; i--) 
+        {            
             dividend.remove(i);
         }
-    
+        
+        // Addinng the crccode to the dividend.
         for ( i = 0; i < (divisor.size() - 1); i++) {
                 
             dividend.add(crccode.get(i));
         }
         
+        // Displaying Codeword,Quotient, Remainder,and CRC code.. 
         System.out.println("| Codeword:  | "+ dividend.toString().replaceAll("[\\[\\],]","")+"\t |");
         System.out.println("| Quotient:  | "+ quotient.toString().replaceAll("[\\[\\],]","")+"\t\t |");
         System.out.println("| Remainder: | "+ remainder.toString().replaceAll("[\\[\\],]","")+"\t\t\t |");
@@ -114,13 +158,15 @@ class CRC {
         Scanner scanner = new Scanner(System.in);
         int i;
         
-
+        // Intialize the siez of dividend.
         System.out.print("\n Enter the size of dividend:\t");
         int sizeofdividend = scanner.nextInt();
 
+        //  Initialize the siez of divisor.
         System.out.print("\n Enter the size of divisor:\t");
         int sizeofdivisor = scanner.nextInt();
         
+        // Initial the dividend list.
         System.out.print("\n Enter the dividend:\t");
         for ( i = 0; i < sizeofdividend; i++){
             
@@ -128,6 +174,7 @@ class CRC {
             k.dividend.add(s1);
         }
         
+        // Initialize the divisor list.
         System.out.print("\n Enter the divisor:\t");
         for ( i = 0; i < sizeofdivisor; i++){
             
@@ -135,15 +182,19 @@ class CRC {
             k.divisor.add(s2);
         }
         
+        // Adding (divisor.size() - 1) zeros to the dividend. 
         for ( i = 0; i < (k.divisor.size() - 1); i++){
             
             k.dividend.add(0);
         }
         
+        //Display dividend and divisor.
         System.out.println("\n");
         System.out.println(" ----------------------------------------");
         System.out.println("| Dividend:  | "+ k.dividend.toString().replaceAll("[\\[\\],]","")+"\t |");
         System.out.println("| Divisor:   | "+ k.divisor.toString().replaceAll("[\\[\\],]","")+"\t\t\t |");
+        
+        // Calling CRC_codegenerator() function.
         k.CRC_codegenerator(sizeofdividend,sizeofdivisor);
         scanner.close();
     }
--- Exp 3/crcdetection.java
+++ Exp 3/crcdetection.java
@@ -2,49 +2,77 @@ import java.util.*;
 
 public class crcdetection {
 
+    //Initialization of ptr as global variable to toggle around the errordetection() function and remain() function effectively.
     private int ptr;
+    
+    // Initialization of dividend and divisor as gloabl list.
     public ArrayList<Integer> dividend = new ArrayList<Integer> ();
     public ArrayList<Integer> divisor = new ArrayList<Integer> ();
 
+    // remain() function.
     public ArrayList<Integer> remain(ArrayList<Integer> remainder,ArrayList<Integer> divid) {
+        //Remove the first elemnt of remainder.
         remainder.remove(0);
+        // Add ptr to remainder.
         remainder.add(dividend.get(ptr));
+        // Clearing divid list for further opearations.
+        divid.clear();
+        // Initialize divid = remainder for further opreations.
         divid = new ArrayList<Integer> (remainder);
+
         return divid;
     }
 
+    // errordetection() function.
     public void errordetection(int sizeofdividend,int sizeofdivisor){
         int i;
-        ArrayList <Integer> divid = new ArrayList <Integer> (dividend);
-        ArrayList <Integer> divir = new ArrayList <Integer> (divisor);
-        ArrayList <Integer> zeros = new ArrayList <Integer> (4);
-        ArrayList <Integer> quotient = new ArrayList <Integer> ();
-        ArrayList <Integer> remainder = new ArrayList <Integer> ();
+        ArrayList <Integer> divid = new ArrayList <Integer> (dividend); // Initializing temporary dividend list for calculation purpose.
+        ArrayList <Integer> divir = new ArrayList <Integer> (divisor); // Initializing temporary dividend list for calculation purpose.
+        ArrayList <Integer> zeros = new ArrayList <Integer> (); // creating a zero list with empty elements.
+        ArrayList <Integer> quotient = new ArrayList <Integer> (); // creating a quotient list to add the quotient elements.
+        ArrayList <Integer> remainder = new ArrayList <Integer> (); // creating a remainder list to add the remainder elements.
 
+        // Adding divisor.size() = 4 numbers of zero to zeros list.
         for (i = 0; i < divisor.size(); i++){
             zeros.add(0);
         }
 
+        // The loop will run till (dividend.size() - divisor.size()) + 1 i.e Example (15 - 6) + 1 = 10 times.
         for (int k = 0; k < (dividend.size() - divisor.size()) + 1; k++){
-            quotient.add(divid.get(0));
+            quotient.add(divid.get(0)); // Adding the first element of divid to quotient.
+            
+            // If first element is 1 the go inside the if statement.
             if (divid.get(0) == 1){
+                // Initialize divir = dividend. Since we will clear divir multiple times so we have initialize divir = divisor when first element of divid is 1.
                 divir = new ArrayList <Integer> (divisor);
 
+                // Applying xor opreation on divid and divr and add it to remainder.
                 for ( i = 0; i < sizeofdivisor; i++){
                     remainder.add( divid.get(i) ^ divir.get(i) );
                 }
 
+                //if it is first iteration when divid = 1 then...
                 if ( k == 0 ){
+                    //initialize ptr = i to add the next elemt of dividend.
                     ptr = i;
+
+                    //Calling remain() function and assigning the value to newly obtained divid.
                     divid = remain(remainder,divid);
+                    //Clearing remainder and divid to initialize the new values for further opreations.
                     remainder.clear();
                     divir.clear();
                     
                 }
-                else{
+                else
+                {
+                    // Incrementing ptr.
                     ptr = this.ptr + 1;
+
+                    // If ptr != dividend.size() then i.e ptr != 15(example) then do the opreations. When ptr == 15 dont do  anything.
                     if (ptr != dividend.size()){
+                        //Calling remain() function and assigning the value to newly obtained divid.
                         divid = remain(remainder,divid);
+                        //Clearing remainder and divid to initialize the new values.
                         remainder.clear();
                         divir.clear();
                         
@@ -52,25 +80,39 @@ public class crcdetection {
                 }
             }
             else {
+                // If the first element of divid is 0 then initialize divir to zero.
                 for (i = 0; i< divisor.size(); i++){
                     divir.add(0);
                 }
 
+                // Applying xor opreation on divid and divr and add it to remainder.
                 for ( i = 0; i < divisor.size(); i++) {
                     remainder.add(divid.get(i) ^ divir.get(i));
                 }
                 
-                if (k == 0){
+                //if it is first iteration when divid = 0 then...
+                if (k == 0)
+                {
+                    //initialize ptr = i to add the next elemt of dividend.
                     ptr = i;
+                    //Calling remain() function and assigning the value to newly obtained divid.
                     divid = remain(remainder,divid);
+                    //Clearing remainder and divid to initialize the new values for further opreations.
                     remainder.clear();
                     divir.clear();
                     
                 }
-                else{
+                else
+                {
+                    // Incrementing ptr.
                     ptr = this.ptr + 1;
-                    if (ptr != dividend.size()){
+
+                    // If ptr != dividend.size() then i.e ptr != 15(example) then do the opreations. When ptr == 15 dont do  anything.
+                    if (ptr != dividend.size())
+                    {
+                        //Calling remain() function and assigning the value to newly obtained divid.
                         divid = remain(remainder,divid);
+                        //Clearing remainder and divid to initialize the new values for further opreations.
                         remainder.clear();
                         divir.clear();
                         
@@ -79,15 +121,19 @@ public class crcdetection {
             }
         }
         
+        // If remainder = 0 then display No error message else display Error detected.
         if (remainder.equals(zeros)){
             System.out.println("\u001B[32mNo error");
             
+            // Initializing the last index of dividend to j.
             int j = dividend.size() - 1;
+            // Remove divisor.size() - 1  bits (Example) 6 - 1 = 5 bits from dividend.
             for (i = 0; i < divisor.size() - 1; i++){
                 dividend.remove(j);
                 j--;
             }
 
+            //Display the Final Data.
             System.out.println("Actual data: "+ dividend.toString().replaceAll("[\\[\\],]","")+"\u001B[39m");
         }
         else {
@@ -101,24 +147,29 @@ public class crcdetection {
         Scanner s = new Scanner(System.in);
         int i;
 
+        // Initializing the size of dividend.
         System.out.println("Enter the size of dividend: ");
         int sizeofdividend = s.nextInt();
 
+        //Initializing the size of divisor.
         System.out.println("Enter size of divisor: ");
         int sizeofdivisor = s.nextInt();
 
+        //Initializing the dividend list.
         System.out.println("Enter the dividend: ");
         for( i = 0; i < sizeofdividend; i++ ){
             int k = s.nextInt();
             cd.dividend.add(k);
         }
 
+        //Initializing divisor list.
         System.out.println("Enter the divisor: ");
         for( i = 0; i < sizeofdivisor; i++ ){
             int k = s.nextInt();
             cd.divisor.add(k);
         }
 
+        // Calling errordetection() function.
         cd.errordetection(sizeofdividend,sizeofdivisor);
         s.close();
     }
